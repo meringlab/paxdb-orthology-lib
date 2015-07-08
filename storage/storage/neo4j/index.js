5af3d16a3bbe6c2426282021cbca1840f511d111
Milan Simonovic <mbsimonovic@gmail.com>
2015-07-08 16:12:42 +0200

    neo4j backend: test-driven proteins import - import nogs

--- storage/storage/neo4j/index.js
+++ storage/storage/neo4j/index.js
@@ -36,12 +36,12 @@ exports.import_proteins = function (proteins, abundances) {
         }
         //can't use txn.index.create, lib doesn't allow data and schema manipulation in the same txn, so:
         var indicesDeferred = when.defer()
-        db.index.create('Protein', 'iid', function (err, index) {
+        db.index.createIfNone('Protein', 'iid', function (err, index) {
             if (err) {
                 indicesDeferred.reject('failed to create iid index for Protein: ' + err)
                 return;
             }
-            db.index.create('Protein', 'eid', function (err, index) {
+            db.index.createIfNone('Protein', 'eid', function (err, index) {
                 if (err) indicesDeferred.reject('failed to create eid index for Protein: ' + err)
                 indicesDeferred.resolve()
             });
@@ -51,6 +51,7 @@ exports.import_proteins = function (proteins, abundances) {
     })
     return d.promise
 }
+
 exports.count = function (label, callback) {
     var d = when.defer()
 
@@ -60,4 +61,60 @@ exports.count = function (label, callback) {
         d.resolve(result.data[0][0])
     });
     return d.promise
+}
+
+exports.import_orthgroups = function (groups) {
+
+//TODO check if array or empty
+    var deferredImport = when.defer()
+    var txn = db.batch();
+
+    function saveGroup(g) {
+        console.log('saving group ' + g.name + ' in ' + g.clade)
+        var saved = when.defer()
+
+        //{"id" :9443, "name": "NOG21051","clade": "PRIMATES", "members": [1803841, 1854701]},
+        var node = txn.save({"levelId": g.id /*, "level": g.clade*/});
+        txn.label(node, "NOG");
+
+        db.query("MATCH (p:Protein) WHERE p.iid IN [" + g.members + "] RETURN p.iid, id(p) ", function (err, results) {
+            if (err) {
+                saved.reject(Error(g.name + ' - failed to find proteins ' + g.members + ': ' + err.message));
+                return
+            }
+            var byIid = {}
+            results.forEach(function (r) {
+                byIid[r['p.iid']] = {"id": r['id(p)']};
+            })
+            g.members.forEach(function (protein_iid) {
+                txn.relate(byIid[protein_iid], g.clade, node);
+            })
+            saved.resolve()
+        });
+        return saved.promise;
+    }
+
+    var taskPromises = groups.map(function (g) {
+        return saveGroup(g)
+    });
+    when.all(taskPromises).then(function (not_used) {
+        console.log('committing transaction')
+        txn.commit(function (err, results) {
+            if (err) {
+                var e = Error("TRANSACTION FAILED: " + err.message);
+                e.results = results;
+                deferredImport.reject(e);
+                return
+            }
+
+            deferredImport.resolve();
+            console.log('transaction completed')
+        })
+    }, function (err) {
+        var e = Error("failed to save one of the groups: " + err.message);
+        e.results = results;
+        deferredImport.reject(e);
+    })
+
+    return deferredImport.promise
 }
