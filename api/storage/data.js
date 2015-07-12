5358c40b601c1dd88bfec6d90019674224d51b6d
Milan Simonovic <mbsimonovic@gmail.com>
2015-07-12 15:56:19 +0200

    taxonomic tree loader

--- api/storage/data.js
+++ api/storage/data.js
@@ -41,6 +41,7 @@ exports.orthgroups = {
 }
 
 exports.speciesTissuesMap = loadSpeciesTissuesMap()
+exports.taxonomicTree = loadTree()
 
 function loadSpeciesTissuesMap() {
     const contents = fs.readFileSync('./data/v4.0/ontology/ontology_2_species.tsv', {'encoding': 'utf8'});
@@ -54,6 +55,43 @@ function loadSpeciesTissuesMap() {
             map[rec[0]] = []
         }
         map[rec[0]].push(rec[1])
+    })
+    return map
+}
+
+function loadTree() {
+    const contents = fs.readFileSync('./data/v4.0/ontology/taxonomy_tree.tsv', {'encoding': 'utf8'});
+    var records = contents.split('\n').filter(function (el) {
+        return el[0] !== '#'
+    });
+    const map = {}
+
+    function getParent(rec) {
+        if (!(rec[1] in map)) {
+            map[rec[1]] = {id: parseInt(rec[1]), name: rec[3].toUpperCase(), children: []}
+        } else {
+            if (!map[rec[1]].hasOwnProperty('children')) {
+                map[rec[1]].children = []
+            }
+        }
+        return map[rec[1]]
+    }
+
+    function getChild(rec) {
+        var child = (rec[0] in map) ? map[rec[0]] : {id: parseInt(rec[0]), name: rec[2].toUpperCase()}
+        map[rec[0]] = child
+        return child
+    }
+
+    records.forEach(function (line) {
+        var rec = line.split('\t');
+        if (rec.length < 4) {
+            return
+        }
+        var parent = getParent(rec)
+        var child = getChild(rec)
+        parent.children.push(child)
+        child.parent = parent
     })
     return map
 }
