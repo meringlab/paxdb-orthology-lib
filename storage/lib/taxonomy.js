e4543686de413de963e8bc45a98b5174074d7f37
Milan Simonovic <mbsimonovic@gmail.com>
2016-03-24 12:45:13 +0100

    build family tree

--- storage/lib/taxonomy.js
+++ storage/lib/taxonomy.js
@@ -7,6 +7,7 @@ exports.taxonomicLevels = taxonomicLevels
 exports.allSpeciesUnder = allSpeciesUnder
 exports.isValidTaxonomicLevel = isValidTaxonomicLevel
 exports.availableTissuesAtTaxonomicLevel = availableTissuesAtTaxonomicLevel
+exports.proteinFamilyTree = proteinFamilyTree
 
 //use union type
 const LEVEL_TYPE = {ORTHGROUP: 'orthgroup', SPECIES: 'species'};
@@ -14,21 +15,114 @@ const LEVEL_TYPE = {ORTHGROUP: 'orthgroup', SPECIES: 'species'};
 var taxonomicMap = loadMap()
 // exports.taxonomicMap = taxonomicMap;
 
-var taxonomicMapById = (function () {
-    var m = {}
-    for (var prop in taxonomicMap) {
-        if (taxonomicMap.hasOwnProperty(prop)) {
-            m[taxonomicMap[prop].name] = taxonomicMap[prop]
-        }
+var taxonomicMapById = (function() {
+  var m = {}
+  for (var prop in taxonomicMap) {
+    if (taxonomicMap.hasOwnProperty(prop)) {
+      m[taxonomicMap[prop].name] = taxonomicMap[prop]
     }
-    return m
+  }
+  return m
 })()
 
+/**
+ *
+ * @param proteins array of protein objects
+ * @param taxonomicLevel LUCA, BACTERIA, EUKARYOTES...
+ * @return {{}} family tree
+ */
+function proteinFamilyTree(proteins, taxonomicLevel) {
+  //todo assert proteins is an array
+  if (!proteins || proteins.length === 0) {
+    return {}
+  }
+  var level = getLevel(taxonomicLevel);
+  //need to clone but can't use JSON.parse/stringify because it's circular
+  var tree = cloneTree(level);
+  var inputSpeciesMap = {}
+  _und.forEach(proteins, function(p) {
+    var speciesId = p.id.split('.')[0];
+    if (!(speciesId in inputSpeciesMap)) {
+      inputSpeciesMap[speciesId] = [];
+    }
+    inputSpeciesMap[speciesId].push(p);
+  })
+  var species = []
+  depthFirstTraversal(tree, function(node) {
+    if (node.type === LEVEL_TYPE.SPECIES && node.id in inputSpeciesMap) {
+      species.push(node);
+    }
+    if (node.type === LEVEL_TYPE.ORTHGROUP) {
+      var i = node.children.length
+      while (i--) {
+        if (node.children[i].type === LEVEL_TYPE.SPECIES
+          && !(node.children[i].id in inputSpeciesMap)) {
+          node.children.splice(i, 1);
+        }
+      }
+      //continue search thru children
+      //only afterwards cleanup this node if empty
+    }
+  }, function(node) {
+    //cleanup empty orthgroups
+    if (node.type === LEVEL_TYPE.ORTHGROUP) {
+      var i = node.children.length
+      while (i--) {
+        if (node.children[i].type === LEVEL_TYPE.ORTHGROUP
+          && node.children[i].children.length === 0) {
+          node.children.splice(i, 1);
+        }
+      }
+    }
+  })
+  _und.forEach(species, function(s) {
+    s.proteins = inputSpeciesMap[s.id]
+  })
+  //sort children so it's easier to test
+  depthFirstTraversal(tree, function(node) {
+    if (node.type === LEVEL_TYPE.ORTHGROUP) {
+      node.children.sort(function(c1, c2) {
+        return c1.id - c2.id
+      });
+    }
+  });
+  return tree;
+}
+
+//doesn't handle Dates!
+function cloneTree(obj) {
+  if (null == obj || "object" != typeof obj) return obj;
+
+  // Handle Date
+  if (obj instanceof Date) {
+    throw new Error('i cant handle Dates!')
+  }
+
+  if (obj instanceof Array) {
+    var copy = [];
+    for (var i = 0, len = obj.length; i < len; i++) {
+      copy[i] = cloneTree(obj[i]);
+    }
+    return copy;
+  }
+
+  if (obj instanceof Object) {
+    var copy = {};
+    for (var attr in obj) {
+      if (obj.hasOwnProperty(attr) && 'parent' !== attr) {
+        copy[attr] = cloneTree(obj[attr]);
+      }
+    }
+    return copy;
+  }
+
+  throw new Error("Unable to copy obj! Its type isn't supported.");
+}
 
 function availableTissuesAtTaxonomicLevel(taxonomicLevel) {
-    return allSpeciesUnder(taxonomicLevel).reduce(function (prev, speciesId) {
-        return _und.union(prev, data.speciesTissuesMap[speciesId]);
-    }, []).sort()
+  return allSpeciesUnder(taxonomicLevel).reduce(function(prev, speciesId) {
+    return _und.union(prev, data.speciesTissuesMap[speciesId]);
+  }, []).sort()
 }
 
 /**
@@ -40,46 +134,49 @@ function availableTissuesAtTaxonomicLevel(taxonomicLevel) {
  * @return {boolean}
  */
 function isValidTaxonomicLevel(speciesId, taxonomicLevel) {
-    return _und.contains(taxonomicLevels(speciesId), taxonomicLevel);
+  return _und.contains(taxonomicLevels(speciesId), taxonomicLevel);
 }
 
 function getLevel(taxonomicLevel) {
-    var level = (typeof(taxonomicLevel) === 'number') ? taxonomicMap[taxonomicLevel] : taxonomicMapById[taxonomicLevel]
-    if (level === undefined) {
-        throw Error("unknown taxonomic level: " + taxonomicLevel)
-    }
-    return level;
+  var level = (typeof(taxonomicLevel) === 'number') ? taxonomicMap[taxonomicLevel] : taxonomicMapById[taxonomicLevel]
+  if (level === undefined) {
+    throw Error("unknown taxonomic level: " + taxonomicLevel)
+  }
+  return level;
 }
 
 function allSpeciesUnder(taxonomicLevel) {
-    var level = getLevel(taxonomicLevel);
-    if (level.type !== LEVEL_TYPE.ORTHGROUP || !level.hasOwnProperty('children')) {
-        throw Error(taxonomicLevel + " is empty!? " + level.name)
-    }
-    var children = []
-    appendLeaves(level, children)
-    return children.sort(function (a, b) {
-        return a > b
-    })
+  var level = getLevel(taxonomicLevel);
+  if (level.type !== LEVEL_TYPE.ORTHGROUP || !level.hasOwnProperty('children')) {
+    throw Error(taxonomicLevel + " is empty!? " + level.name)
+  }
+  var children = []
+  appendLeaves(level, children)
+  return children.sort(function(a, b) {
+    return a > b
+  })
 }
 
-function depthFirstTraversal(root, visitor) {
-    visitor(root);
-    if (root.children) {
-        root.children.forEach(function(child) {
-            depthFirstTraversal(child, visitor);
-        });
-    }
+function depthFirstTraversal(root, visitor, postVistor) {
+  visitor(root);
+  if (root.children) {
+    root.children.forEach(function(child) {
+      depthFirstTraversal(child, visitor);
+    });
+  }
+  if (postVistor) {
+    postVistor(root)
+  }
 }
 
 function appendLeaves(level, children) {
-    depthFirstTraversal(level, function(node) {
-        //node.type === 'species' ?
-        // if (!node.hasOwnProperty('children')) {
-        if (node.type === LEVEL_TYPE.SPECIES) {
-            children.push(node.id)
-        }
-    })
+  depthFirstTraversal(level, function(node) {
+    //node.type === 'species' ?
+    // if (!node.hasOwnProperty('children')) {
+    if (node.type === LEVEL_TYPE.SPECIES) {
+      children.push(node.id)
+    }
+  })
 }
 
 /**
@@ -87,63 +184,64 @@ function appendLeaves(level, children) {
  * @return ordered list of all taxonomic levels for this species
  */
 function taxonomicLevels(speciesId) {
-    if (!(speciesId in taxonomicMap)) {
-        //throw Error("unknown species " + speciesId + ", we only have: " + )
-        throw Error("unknown species " + speciesId)
-    }
-    var species = taxonomicMap[speciesId];
-    if (species.hasOwnProperty('children')) {
-        throw Error("not species (but taxonomic level): " + species.name)
-    }
-    var levels = []
-    for (var parent = species.parent; parent !== undefined; parent = parent.parent) {
-        levels.push(parent.name)
-    }
-    levels.reverse()
-    return levels
+  if (!(speciesId in taxonomicMap)) {
+    //throw Error("unknown species " + speciesId + ", we only have: " + )
+    throw Error("unknown species " + speciesId)
+  }
+  var species = taxonomicMap[speciesId];
+  if (species.hasOwnProperty('children')) {
+    throw Error("not species (but taxonomic level): " + species.name)
+  }
+  var levels = []
+  for (var parent = species.parent; parent !== undefined; parent = parent.parent) {
+    levels.push(parent.name)
+  }
+  levels.reverse()
+  return levels
 }
 
 
 function loadMap() {
-    const contents = fs.readFileSync('./data/v4.0/ontology/taxonomy_tree.tsv', {'encoding': 'utf8'});
-    var records = contents.split('\n').filter(function (el) {
-        return el[0] !== '#'
-    });
-    const map = {}
+  const contents = fs.readFileSync('./data/v4.0/ontology/taxonomy_tree.tsv', {'encoding': 'utf8'});
+  var records = contents.split('\n').filter(function(el) {
+    return el[0] !== '#'
+  });
+  const map = {}
 
-    function getType(id) {
-        return id in data.orthgroups ? LEVEL_TYPE.ORTHGROUP : LEVEL_TYPE.SPECIES;
-    }
-    function getParent(rec) {
-        if (!(rec[1] in map)) {
-            map[rec[1]] = {id: parseInt(rec[1]), name: rec[3].toUpperCase(), type: getType(rec[1]), children: []}
-        } else {
-            if (!map[rec[1]].hasOwnProperty('children')) {
-                map[rec[1]].children = []
-            }
-        }
-        return map[rec[1]]
-    }
+  function getType(id) {
+    return id in data.orthgroups ? LEVEL_TYPE.ORTHGROUP : LEVEL_TYPE.SPECIES;
+  }
 
-    function getChild(rec) {
-        var child = (rec[0] in map) ? map[rec[0]] : {
-            id: parseInt(rec[0]),
-            name: rec[2].toUpperCase(),
-            type: getType(rec[0])
-        }
-        map[rec[0]] = child
-        return child
+  function getParent(rec) {
+    if (!(rec[1] in map)) {
+      map[rec[1]] = {id: parseInt(rec[1]), name: rec[3].toUpperCase(), type: getType(rec[1]), children: []}
+    } else {
+      if (!map[rec[1]].hasOwnProperty('children')) {
+        map[rec[1]].children = []
+      }
     }
+    return map[rec[1]]
+  }
 
-    records.forEach(function (line) {
-        var rec = line.split('\t');
-        if (rec.length < 4) {
-            return
-        }
-        var parent = getParent(rec)
-        var child = getChild(rec)
-        parent.children.push(child)
-        child.parent = parent
-    })
-    return map
+  function getChild(rec) {
+    var child = (rec[0] in map) ? map[rec[0]] : {
+      id: parseInt(rec[0]),
+      name: rec[2].toUpperCase(),
+      type: getType(rec[0])
+    }
+    map[rec[0]] = child
+    return child
+  }
+
+  records.forEach(function(line) {
+    var rec = line.split('\t');
+    if (rec.length < 4) {
+      return
+    }
+    var parent = getParent(rec)
+    var child = getChild(rec)
+    parent.children.push(child)
+    child.parent = parent
+  })
+  return map
 }
--- storage/test/taxonomy.js
+++ storage/test/taxonomy.js
@@ -2,42 +2,95 @@
  * Created by milans on 13/07/15.
  */
 var should = require('chai').should(),
-    expect = require('chai').expect,
-    assert = require('assert');
+  expect = require('chai').expect,
+  assert = require('assert');
 
 
 var taxonomy = require('../lib/taxonomy')
 
-describe('taxonomy', function () {
-    it('should return all taxonomic levels in ascending order for a species', function () {
-        taxonomy.taxonomicLevels(198214/*shigella*/).should.deep.equal(['LUCA',
-            'BACTERIA',
-            'PROTEOBACTERIA',
-            'GAMMAPROTEOBACTERIA',
-            'ENTEROBACTERIACEAE'])
-        taxonomy.taxonomicLevels(9606).should.deep.equal(['LUCA',
-            'EUKARYOTES',
-            'OPISTHOKONTS',
-            'METAZOA',
-            'COELOMATA',
-            'CHORDATES',
-            'SARCOPTERYGII',
-            'AMNIOTES',
-            'MAMMALS',
-            'EUARCHONTOGLIRES',
-            'PRIMATES'
-        ])
-    })
-    it('should return all species under a taxonomic level', function () {
-        taxonomy.allSpeciesUnder('PRIMATES').should.deep.equal([9598, 9606])
-        taxonomy.allSpeciesUnder('RODENTS').should.deep.equal([10090, 10116])
-        taxonomy.allSpeciesUnder('EUARCHONTOGLIRES').should.deep.equal([9598, 9606, 10090, 10116])
-    })
-    it('should return all tissues under a taxonomic level', function () {
-        taxonomy.availableTissuesAtTaxonomicLevel('BACTERIA').should.deep.equal(['WHOLE_ORGANISM'])
-        taxonomy.availableTissuesAtTaxonomicLevel('PRIMATES')[0].should.equal('ADRENAL_GLAND')
-        taxonomy.availableTissuesAtTaxonomicLevel('RODENTS')[0].should.equal('ADRENAL_GLAND')
+describe('taxonomy', function() {
+  it('should return all taxonomic levels in ascending order for a species', function() {
+    taxonomy.taxonomicLevels(198214/*shigella*/).should.deep.equal([
+      'LUCA',
+      'BACTERIA',
+      'PROTEOBACTERIA',
+      'GAMMAPROTEOBACTERIA',
+      'ENTEROBACTERIACEAE'
+    ])
+    taxonomy.taxonomicLevels(9606).should.deep.equal([
+      'LUCA',
+      'EUKARYOTES',
+      'OPISTHOKONTS',
+      'METAZOA',
+      'COELOMATA',
+      'CHORDATES',
+      'SARCOPTERYGII',
+      'AMNIOTES',
+      'MAMMALS',
+      'EUARCHONTOGLIRES',
+      'PRIMATES'
+    ])
+  })
+  it('should return all species under a taxonomic level', function() {
+    taxonomy.allSpeciesUnder('PRIMATES').should.deep.equal([9598, 9606])
+    taxonomy.allSpeciesUnder('RODENTS').should.deep.equal([10090, 10116])
+    taxonomy.allSpeciesUnder('EUARCHONTOGLIRES').should.deep.equal([9598, 9606, 10090, 10116])
+  })
+  it('should return all tissues under a taxonomic level', function() {
+    taxonomy.availableTissuesAtTaxonomicLevel('BACTERIA').should.deep.equal(['WHOLE_ORGANISM'])
+    taxonomy.availableTissuesAtTaxonomicLevel('PRIMATES')[0].should.equal('ADRENAL_GLAND')
+    taxonomy.availableTissuesAtTaxonomicLevel('RODENTS')[0].should.equal('ADRENAL_GLAND')
+  })
+  describe('family tree', function() {
+    it('root should be at the requested level', function() {
+      var tree = taxonomy.proteinFamilyTree([
+        {'id': '4932.YHR174W'}
+      ], 'SACCHAROMYCETA');
+      tree.name.should.equal('SACCHAROMYCETA');
     })
 
+    it('should only have the requested species', function() {
+      var tree = taxonomy.proteinFamilyTree([
+        {'id': '4932.YHR174W'}
+      ], 'SACCHAROMYCETA');
+      tree.children.length.should.equal(1);
+      tree.children[0].id.should.equal(4932);
 
+      var tree = taxonomy.proteinFamilyTree([
+        {'id': '5061.CADANGAP00013996'}
+      ], 'SACCHAROMYCETA');
+      tree.children.length.should.equal(1);
+      tree.children[0].id.should.equal(5061);
+
+      var tree = taxonomy.proteinFamilyTree([
+        {'id': '4932.YGR254W'},
+        {'id': '5061.CADANGAP00013996'}
+      ], 'SACCHAROMYCETA');
+      tree.name.should.equal('SACCHAROMYCETA');
+      tree.children.length.should.equal(2);
+      tree.children[0].id.should.equal(4932);
+      tree.children[1].id.should.equal(5061);
+    })
+
+    it('should recognise when species is at the same level as orthgroup', function() {
+      var tree = taxonomy.proteinFamilyTree([
+        {'id': '4932.YGR254W'},
+        {'id': '214684.someprot'}
+      ], 'FUNGI');
+      tree.children.length.should.equal(2);
+      tree.children[1].id.should.equal(214684);
+      var ascomycetes = tree.children[0];
+      ascomycetes.id.should.equal(4890);
+      ascomycetes.children[0].id.should.equal(716545);
+    })
+
+    it('should have proteins assigned to species', function() {
+      var tree = taxonomy.proteinFamilyTree([
+        {'id': '4932.YHR174W'}
+      ], 'SACCHAROMYCETA');
+      var yeast = tree.children[0];
+      yeast.proteins.length.should.equal(1);
+      yeast.proteins[0].id.should.equal('4932.YHR174W');
+    })
+  })
 })
