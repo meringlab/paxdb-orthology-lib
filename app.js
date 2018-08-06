65f574e8659239a3b03b53e131eafe8235f7596f
Milan Simonovic <mbsimonovic@gmail.com>
2018-08-06 14:29:18 +0200

    remove unused, add favicon

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var groupRoute = require('./routes/groups');
@@ -14,15 +11,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev')); // or app.use(morgan('combined'))
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//TODO serve static resource with nginx
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use('/', routes);
 app.use('/protein', proteinRoute);
